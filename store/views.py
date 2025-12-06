bd09ac25d0e0d4f56ed10a9ed57742cd8fc72124
SiddharthSundram <155897849+SiddharthSundram@users.noreply.github.com>
2025-12-07 04:50:50 +0530

    Product detail page and order request form implemented

--- store/views.py
+++ store/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, OrderRequest
 
 def home(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    if request.method == "POST":
+        name = request.POST['name']
+        phone = request.POST['phone']
+        address = request.POST['address']
+
+        OrderRequest.objects.create(
+            customer_name=name,
+            customer_phone=phone,
+            customer_address=address,
+            product=product
+        )
+        return render(request, 'store/success.html')
+
+    return render(request, 'store/product_detail.html', {'product': product})
